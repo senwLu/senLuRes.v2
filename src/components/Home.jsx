87b6b99cef03b5e6b70bd61dfc7149f2c215da0e
Sen Lu <swei.lu1018@gmail.com>
2020-11-25 03:21:37 -0800

    picked a style and added css to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import space from '../images/space4.jpg';
+
 
 const Home = () => {
   return (
+
    <div className='container'>
       <div className='homeIMG-box'>
          <div className='homeIMG-innerBox'>
             <div className='homeImg'></div> 
          </div>   
       </div>
-      <div className='homeNav-box'>
-         <div className='homeNav-text'>
-            <div className='homeNav-nav'>
-               <div className='homeNav-nav-btn'>ABOUT</div>
-               <div className='homeNav-nav-btn'>PROJECT/S</div>
-               <div className='homeNav-nav-btn'>CONTACT</div>
+
+      <div className='homeNav-container'>
+         <div className='homeNav-box'>
+            <div className='homeNav-innerBox'>
+               <div className='homeNav-nav'>
+                  <div>
+                     <p>
+                        Full-Stack
+                        <br/>
+                        Developer
+                     </p>
+                  </div>
+                  <div data-text='text' className='homeNav-nav-link change-to-outline'>ABOUT</div>
+                  <div className='homeNav-nav-link change-to-outline'>PROJECT/S</div>
+                  <div className='homeNav-nav-link change-to-outline'>CONTACT</div>
+               </div>
             </div>
-         </div>
+      </div>
+
+      
+         
       </div>
    </div>
   )
