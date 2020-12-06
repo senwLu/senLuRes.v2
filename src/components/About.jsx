13b0223b4207d1154218e18d1ded3205576859a1
Sen Lu <swei.lu1018@gmail.com>
2020-12-06 15:19:38 -0800

    made changed to about => css & component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,28 +4,27 @@ import React from 'react';
 const About = () => {
   	return (
     	<div className='about-container' >
-			<h1 className='slide-right'>ABOUT</h1>
-			<div className='aboutme-wrapper slide-left'>
-			<p>
-					Hi, I'm Sen - a <span className='about-style'>Full-Stack Software Engineer </span> 
-					that graduated from <span className='about-style'>Hack Reactor @ Galvanize</span>.
-					I am a <span className='about-style'>Sci-Fi</span> nerd at heart that loves drinking 
-					<span className='about-style'> coffee</span> and talking about tech/bio 
-					<span className='about-style'> startups</span>. Currently, I am based out of
-					<span className='about-style'> Seattle</span> and is looking for new and exciting opportunities.
-			</p>
+			<h1>ABOUT</h1>
+
+			<div className='aboutme-wrapper'>
+				<p>
+					Hi, I'm Sen - a <span className='style-color'>Full-Stack Software Engineer </span> 
+					that graduated from <span className='style-color'>Hack Reactor @ Galvanize</span>.
+					I am a <span className='style-color'>Sci-Fi</span> nerd at heart that loves drinking 
+					<span className='style-color'> coffee</span> and talking about tech/bio 
+					<span className='style-color'> startups</span>. Currently, I am based out of
+					<span className='style-color'> Seattle</span> and is looking for new and exciting opportunities.
+				</p>
 			</div>
-			<div className='row row-container'>
-				<div className='column c1'>
-						<div className='aboutskill-wrapper slide-top'>
-							<p><span className='about-style'>Education</span>&nbsp;&nbsp;Seattle University + Hack Reactor @ Galvanize</p>
-							<p><span className='about-style'>Languages</span>&nbsp;&nbsp;JavaScript, HTML, CSS</p>
-							<p><span className='about-style'>Database</span>&nbsp;&nbsp;MongoDB, MySQL, PostgreSQL</p>
-							<p><span className='about-style'>Frameworks</span>&nbsp;&nbsp;Express, ReactJS, NodeJS</p>
-							<p><span className='about-style'>Testing</span>&nbsp;&nbsp;Jest, Enzyme, Mocha-Chai</p>
-						</div>
-				</div>
+
+			<div>
+				<p><span className='style-color'>Education</span>&nbsp;&nbsp;Seattle University + Hack Reactor @ Galvanize</p>
+				<p><span className='style-color'>Languages</span>&nbsp;&nbsp;JavaScript, HTML, CSS</p>
+				<p><span className='style-color'>Database</span>&nbsp;&nbsp;MongoDB, MySQL, PostgreSQL</p>
+				<p><span className='style-color'>Frameworks</span>&nbsp;&nbsp;Express, ReactJS, NodeJS</p>
+				<p><span className='style-color'>Testing</span>&nbsp;&nbsp;Jest, Enzyme, Mocha-Chai</p>
 			</div>
+		
     	</div>
   	)
 }
