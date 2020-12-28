e9cb6dd32103c9c1ee8855070fa5ef442868c85c
Sen Lu <swei.lu1018@gmail.com>
2020-12-28 14:38:53 -0800

    removed project placeholders

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,28 +11,6 @@ const Project = () => {
 					<p className='project-box-title'>CalculatorJS</p>
 					<p className='project-box-detail'>Tools used: JavaScript, ReactJS, Webpacks</p>
 				</a>
-
-
-				<div className='project-box'>
-					<p>Placeholder 2</p>
-				</div>
-
-				<div className='project-box'>
-					<p>Placeholder 3</p>
-				</div>
-
-				<div className='project-box'>
-					<p>Placeholder 4</p>
-				</div>
-
-				<div className='project-box'>
-					<p>Placeholder 5</p>
-				</div>
-
-				<div className='project-box'>
-					<p>Placeholder 6</p>
-				</div>
-
 			</div>
 
     	</div>
